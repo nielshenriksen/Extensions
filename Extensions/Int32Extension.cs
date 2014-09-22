40478eddd5a0fcb8aa5548e1d1405e6d6db0dd5f
Niels Henriksen <niels@niels-henriksen.dk>
2014-09-22 10:10:18 +0200

    Added Int32 extension

---
+++ Extensions/Int32Extension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class Int32Extension
+    {
+        public static string PadZero(this int sValue, int iMaxLength)
+        {
+            var fmt = new String('0', iMaxLength) + ".##";
+            string formatString = "{0," + iMaxLength + ":" + fmt + "}";
+
+            return String.Format(formatString, sValue);
+        }
+    }
+}
