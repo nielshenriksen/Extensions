7d1ea911e25eb32dd34c1d03c9dbf68a4297f79c
Niels Henriksen <niels@niels-henriksen.dk>
2014-09-22 10:10:27 +0200

    Added StringBuilder extension

---
+++ Extensions/StringBuilderExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class StringBuilderExtension
+    {
+        public static StringBuilder AppendLineFormat(this StringBuilder builder, string format, params object[] arguments)
+        {
+            string value = String.Format(format, arguments);
+
+            builder.AppendLine(value);
+
+            return builder;
+        }
+    }
+}
