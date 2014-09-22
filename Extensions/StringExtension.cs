ae9169035f541730578d6d3df3d1059208d48e14
Niels Henriksen <niels@niels-henriksen.dk>
2014-09-22 10:06:32 +0200

    Started the project

---
+++ Extensions/StringExtension.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class StringExtension
+    {
+        public static string Right(this string sValue, int iMaxLength)
+        {
+            //Check if the value is valid
+            if (string.IsNullOrEmpty(sValue))
+            {
+                //Set valid empty string as string could be null
+                sValue = string.Empty;
+            }
+            else if (sValue.Length > iMaxLength)
+            {
+                //Make the string no longer than the max length
+                sValue = sValue.Substring(sValue.Length - iMaxLength, iMaxLength);
+            }
+
+            //Return the string
+            return sValue;
+        }
+
+        public static bool IsNull(this string source)
+        {
+            return source == null;
+        }
+
+        public static bool IsValidEmailAddress(this string s)
+        {
+            var regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            return regex.IsMatch(s);
+        }
+
+        public static Int32 ToInt32(this string s)
+        {
+            return Int32.Parse(s);
+        }
+    }
+}
---
+++ UnitTest/StringExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Extensions;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class StringExtension
+    {
+        [TestMethod]
+        public void String_Right()
+        {
+            var input = "Hello World";
+            var expected = "World";
+
+            Assert.AreEqual(expected, input.Right(expected.Length));
+        }
+
+        [TestMethod]
+        public void String_Is_Not_Null()
+        {
+            var input = "This string is not null";
+
+            Assert.AreEqual(false, input.IsNull());
+        }
+
+        [TestMethod]
+        public void String_Is_Null()
+        {
+            string input = null;
+
+            Assert.AreEqual(true, input.IsNull());
+        }
+    }
+}
