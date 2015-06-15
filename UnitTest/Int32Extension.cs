92033799746e8594b521eb1fae6b62263d387471
Niels Henriksen <niels@niels-henriksen.dk>
2015-06-15 10:02:54 +0200

    Added UnitTest for Int32

---
+++ UnitTest/Int32Extension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Extensions;
+
+
+namespace UnitTest
+{
+    [TestClass]
+    public class Int32Extension
+    {
+        [TestMethod]
+        public void Date28thAugust1971()
+        {
+            var day = 28;
+
+            var testdate = new DateTime(1971, 8, 28);
+
+            var date = day.August(1971);
+
+            Assert.AreEqual(testdate, date);
+        }
+    }
+}
